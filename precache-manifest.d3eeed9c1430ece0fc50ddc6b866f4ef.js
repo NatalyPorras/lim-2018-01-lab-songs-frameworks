6f7055f03fa8ffe1e92d3a92f609b23aa6daba12
Naty <natalypc27@gmail.com>
2018-10-09 15:11:00 -0500

    Updates

---
+++ precache-manifest.d3eeed9c1430ece0fc50ddc6b866f4ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8b6b38dd23eb6f56209e",
+    "url": "/static/js/main.8b6b38dd.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "8b6b38dd23eb6f56209e",
+    "url": "/static/css/main.3c61583a.chunk.css"
+  },
+  {
+    "revision": "a897551a4b89a7b3b62a11869b7c0fc1",
+    "url": "/index.html"
+  }
+];
